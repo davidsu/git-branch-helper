76e16e27fb5406a55af42a4b040b32b1fd497e1f
David <davidsu@wix.com>
2015-12-06 21:09:12 +0200

    param file created

---
+++ src/utils/params.js
@@ -0,0 +1,17 @@
+/**
+ * Created by davidsu on 06/12/2015.
+ */
+
+var _ = require('lodash-node');
+var params = _.chain(process.argv)
+    .filter((arg)=>_.startsWith(arg, '-') && _.contains(arg, '='))
+    .map((arg)=>{
+        var i = _.startsWith(arg, '--')?2:1;
+        return arg.toLowerCase().substring(i).split('=');
+    })
+    .zipObject()
+    .value();
+
+console.log('params', JSON.stringify(params));
+module.exports.branch = params.b || params.branch;
+
