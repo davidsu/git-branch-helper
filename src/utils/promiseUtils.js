4bc0f1b5025a43b09256462414e1ed73b82dfa59
David <davidsu@wix.com>
2015-12-06 14:10:45 +0200

    logtask defaultReject

--- src/utils/promiseUtils.js
+++ src/utils/promiseUtils.js
@@ -4,6 +4,7 @@
 var chalk = require('chalk');
 var log = require('./logUtils');
 function defaultReject(rejectObj) {
+    log.task('defaultReject');
     rejectObj.err && log.err(rejectObj.err);
     rejectObj.stderr && log.info(chalk.cyan('STDERR'));
     rejectObj.stderr && log.err(rejectObj.stderr);
