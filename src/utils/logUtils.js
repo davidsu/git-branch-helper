4adf59369de9837fa56dc0a18301500e4a90a2a7
David <davidsu@wix.com>
2015-12-06 14:32:04 +0200

    refator: improve naming

--- src/utils/logUtils.js
+++ src/utils/logUtils.js
@@ -5,7 +5,7 @@ var chalk = require('chalk');
 
 var log = console.log;
 module.exports = console.log;
-module.exports.errLog = (err)=>log(chalk.red(err));
+module.exports.err = (err)=>log(chalk.red(err));
 module.exports.task = (msg)=>log(chalk.underline('\n'+msg));
 module.exports.info = (msg)=>log(chalk.blue(msg));
 module.exports.greenLog = (msg)=>log(chalk.green(msg));
