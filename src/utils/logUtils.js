e0c98e3f7d514e12bd1ebf325c7e29c778e9c8d4
David <davidsu@wix.com>
2015-12-06 08:43:57 +0200

    something went wrong

--- src/utils/logUtils.js
+++ src/utils/logUtils.js
@@ -5,4 +5,6 @@ var chalk = require('chalk');
 
 module.exports.log = console.log;
 module.exports.errLog = (err)=>log(chalk.red(err));
+module.exports.task = (msg)=>log(chalk.underline(msg));
+module.exports.info = (msg)=>log(chalk.blue(msg));
 module.exports.greenLog = (msg)=>log(chalk.green(msg));
